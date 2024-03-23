026739dfd65293cf9e7c99b7b97573ebd76c6d64
Samira <samirasaad@gmail.com>
2024-03-23 20:19:17 +0200

    handle remove product from cart from product details

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromCart } from "../../store/featuresSlices/cart";
 
-const Cart =() => {
-    return (
-        <p>cart</p>
-    )
-}
+const Cart = () => {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector(({ cart }) => cart);
 
-export default Cart
+  const handleRemoveFromCart = (productId) => {
+    dispatch(removeFromCart(productId));
+  };
+
+  return (
+    <div>
+      {cartItems?.map((item) => (
+        <p key={item.id}>
+          {item.title}
+          <button onClick={() => handleRemoveFromCart(item.id)}>remove</button>
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Cart;
