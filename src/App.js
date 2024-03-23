5870aa3abf37eea814439fd2939ea0616de16bc0
Samira <samirasaad@gmail.com>
2024-03-23 22:52:40 +0200

    adding component for loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routing/router";
+import "./App.css";
 
-const App =()=> {
+const App = () => {
   return (
     <div className="App">
+      <div id="primary-spinner-bg"></div>
       <RouterProvider router={router} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <p style={{ display: "flex", justifyContent: "center" }}>
+      loading..................
+    </p>
+  );
+};
+
+export default Loader;
