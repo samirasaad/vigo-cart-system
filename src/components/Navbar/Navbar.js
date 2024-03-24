cf5d01d10364725864fa8056ff6bca449e414321
Samira <samirasaad@gmail.com>
2024-03-24 13:09:07 +0200

    handle cart number in navbar according to Qty number of all items not items array length

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [numberOfItems, setNumberOfItems] = useState(0);
   const { cartItems } = useSelector(({ cart }) => cart);
+
+  useEffect(() => {
+    if (cartItems.length) {
+      setNumberOfItems(calculateNumberOfItems(cartItems));
+    }
+  }, [cartItems]);
+
+  const calculateNumberOfItems = (cartItems, index = 0) => {
+    // if index reaches the end of the cart list => return 0
+    if (index >= cartItems.length) {
+      return 0;
+    }
+
+    // recursion to add the quantity of the current item to the total of previous quantities,
+    const currentItem = cartItems[index];
+    let totalItems = 0;
+    totalItems += currentItem.Qty;
+    return totalItems + calculateNumberOfItems(cartItems, index + 1);
+  };
+
   return (
     <div>
       <Link to="/">Products</Link>
       <Link to="/cart">
         Cart
-        {cartItems?.length && <span>{cartItems.length}</span>}
+        {numberOfItems && <span>{numberOfItems}</span>}
       </Link>
     </div>
   );
